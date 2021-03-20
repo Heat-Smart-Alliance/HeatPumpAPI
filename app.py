bae5553aa634caaf810c63f6e86310ee79a14bd6
Daniel Hariyanto <dthariyanto@gmail.com>
2021-03-20 13:22:39 -0400

    test endpoint

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return "Hello world!"
+    return {message: "Hello World}
 
 if __name__ == "__main__":
     app.run(debug=True)
