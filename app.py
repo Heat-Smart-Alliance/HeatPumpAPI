526555e2b1730d68ed4c1eb0aa4ec1f61ce34c9c
Daniel Hariyanto <dthariyanto@gmail.com>
2021-03-20 13:47:36 -0400

    test endpoint

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['POST', 'GET'])
 def index():
+    data_input = request.json
+    return jsonify(data_input)
+
+@app.route('/test', methods=['GET'])
+def test():
     return {'message': "Hello World"}
 
 if __name__ == "__main__":
